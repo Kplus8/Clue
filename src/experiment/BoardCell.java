620bfebdc82f217cfca44c871b143db46f0eeacc
James DeBlock <jdeblock@mines.edu>
2018-02-27 16:18:19 -0700

    Part 1 completed

---
+++ src/experiment/BoardCell.java
@@ -0,0 +1,21 @@
+package experiment;
+
+/**
+ * 
+ * @author Jim DeBlock
+ * @author Graham Kitchenka
+ *
+ */
+
+public class BoardCell {
+
+	private int row, column;
+	
+	public BoardCell (int row, int column) {
+		
+		this.row = row;
+		this.column = column;
+		
+	}
+	
+}
