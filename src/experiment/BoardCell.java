813e076d2346ffa334793bc6aa6ccf4c8f0a4486
James DeBlock <jdeblock@mines.edu>
2018-02-27 17:43:38 -0700

    Almost completed Part 2 - Instance Errors

--- src/experiment/BoardCell.java
+++ src/experiment/BoardCell.java
@@ -10,12 +10,31 @@ package experiment;
 public class BoardCell {
 
 	private int row, column;
-	
-	public BoardCell (int row, int column) {
-		
+
+	public BoardCell(int row, int column) {
+
 		this.row = row;
 		this.column = column;
-		
+
 	}
-	
+
+	public int getRow() {
+		return row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	@Override
+	public String toString() {
+		return "(" + row + ", " + column + ")";
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		BoardCell c = (BoardCell) o;
+		return c.getRow() == this.getRow() && this.getColumn() == c.getColumn();
+	}
+
 }
