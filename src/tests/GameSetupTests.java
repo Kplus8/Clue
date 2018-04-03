698114375114ae60f5d7cb61aca596130d7bb011
James DeBlock <jdeblock@mines.edu>
2018-04-03 16:44:28 -0600

    Added empty tests for GameSetup

--- src/tests/GameSetupTests.java
+++ src/tests/GameSetupTests.java
@@ -1,5 +1,10 @@
 package tests;
 
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import clueGame.Board;
+
 /**
  * 
  * @author Jim DeBlock
@@ -10,6 +15,58 @@ package tests;
 
 public class GameSetupTests {
 
+	private static Board board;
+
+	@BeforeClass
+	public static void setUp() {
+		// Board is singleton, get the only instance
+		board = Board.getInstance();
+		// set the file names to use my config files
+		board.setConfigFiles("data\\Map.csv", "data\\ClueRooms.txt");
+		// Initialize will load BOTH config files
+		board.initialize();
+	}
+
+	@Test
+	public void testLoadingPeople() {
+
+		
+		
+	}
+
+	@Test
+	public void testLoadDeck() {
+
+	}
+
+	@Test
+	public void testDealCards() {
+
+	}
+
+	@Test
+	public void testTargetLocation() {
+
+	}
+
+	@Test
+	public void testAccusation() {
+
+	}
+
+	@Test
+	public void testCreateSuggestion() {
+
+	}
+
+	@Test
+	public void testDisproveSuggestion() {
+		
+	}
 	
+	@Test
+	public void testPlayerDisproveSuggestion() {
+		
+	}
 	
 }
